4ac8a60ca531f0cf826c0f41bc4e9d6f1cd02ffe
Zahar <melnik.zakhar222@gmail.com>
2020-01-24 14:52:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
 	  $(this).siblings('.text').slideDown(200);
 	  }
 	});
+
 });
 
 window.onscroll = function() {myFunction()};
@@ -60,3 +61,22 @@ $(".container-popup").click(function(){
 $(".create-api-key .close").click(function(){
   $(".container-popup").removeClass("show");
 });
+
+
+
+
+jQuery(window).scroll(function(){
+         var $sections = $('section');
+	$sections.each(function(i,el){
+        var top  = $(el).offset().top-100;
+        var bottom = top +$(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+    	if( scroll > top && scroll < bottom){
+            $('a.active').removeClass('active');
+			$('a[href="#'+id+'"]').addClass('active');
+
+        }
+    })
+ });
+
